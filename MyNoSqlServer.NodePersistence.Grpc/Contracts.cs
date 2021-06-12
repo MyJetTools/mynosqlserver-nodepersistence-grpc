bcdde1ccca014737b73c6e8317b667cc3c77ebad
Andrey <amigin@gmail.com>
2021-06-12 11:00:25 +0300

    First Commit

---
+++ MyNoSqlServer.NodePersistence.Grpc/Contracts.cs
@@ -0,0 +1,68 @@
+using System.Runtime.Serialization;
+
+namespace MyNoSqlServer.NodePersistence.Grpc
+{
+    [DataContract]
+    public class PayloadWrapperGrpcModel
+    {
+        [DataMember(Order = 1)]
+        public byte[] Payload { get; set; }
+    }
+    
+    
+    [DataContract]
+    public class SaveSnapshotGrpcModel
+    {
+        [DataMember(Order = 1)]
+        public string TableName { get; set; }
+        
+        [DataMember(Order = 2)]
+        public byte[] CompressedContent { get; set; }
+    }
+
+    [DataContract]
+    public class DeleteTablePartitionGrpcRequest
+    {
+        [DataMember(Order = 1)]
+        public string TableName { get; set; }
+        
+        [DataMember(Order = 2)]
+        public string[] PartitionKeys { get; set; }
+    }
+
+    [DataContract]
+    public class SetTableAttributesGrpcRequest
+    {
+        [DataMember(Order = 1)]
+        public string TableName { get; set; }
+        
+        [DataMember(Order = 2)]
+        public bool Persist { get; set; }
+
+        [DataMember(Order = 3)]
+        public int MaxPartitionsAmount { get; set; }
+    }
+    
+    
+    [DataContract]
+    public class GetTableGrpcResponse
+    {
+        [DataMember(Order = 1)]
+        public string TableName { get; set; }
+        
+        [DataMember(Order = 2)]
+        public bool Persist { get; set; }
+
+        [DataMember(Order = 3)]
+        public int MaxPartitionsAmount { get; set; }
+    }
+
+    [DataContract]
+    public class DownloadTableGrpcRequest
+    {
+        [DataMember(Order = 1)]
+        public string TableName { get; set; }
+    }
+    
+
+}
---
+++ MyNoSqlServer.NodePersistence.Grpc/IMyNoSqlServerNodePersistenceGrpcService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace MyNoSqlServer.NodePersistence.Grpc
+{
+    [ServiceContract(Name = "persistence.MyNoSqlServer")]
+    public interface IMyNoSqlServerNodePersistenceGrpcService
+    {
+
+        [OperationContract(Action = "SaveTableSnapshot")]
+        ValueTask SaveTableSnapshotAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
+        
+        [OperationContract(Action = "SavePartitionSnapshot")]
+        ValueTask SavePartitionSnapshotAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
+        
+        [OperationContract(Action = "DeleteTablePartitions")]
+        ValueTask DeleteTablePartitionsAsync(DeleteTablePartitionGrpcRequest request);
+
+        [OperationContract(Action = "SetTableAttributes")]
+        ValueTask SetTableAttributesAsync(SetTableAttributesGrpcRequest request);
+
+        [OperationContract(Action = "GetTables")]
+        IAsyncEnumerable<GetTableGrpcResponse> GetTablesAsync();
+
+        [OperationContract(Action = "DownloadTable")]
+        ValueTask<PayloadWrapperGrpcModel> DownloadTableAsync(DownloadTableGrpcRequest request);
+
+    }
+}
