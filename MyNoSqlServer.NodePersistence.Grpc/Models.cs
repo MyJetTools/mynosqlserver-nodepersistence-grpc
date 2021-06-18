9c42851a79772ae5c10ba2ce735017d57e18c992
Andrey <amigin@gmail.com>
2021-06-18 22:30:22 +0300

    Added Update RowsContent

--- MyNoSqlServer.NodePersistence.Grpc/Models.cs
+++ MyNoSqlServer.NodePersistence.Grpc/Models.cs
@@ -27,18 +27,40 @@ namespace MyNoSqlServer.NodePersistence.Grpc
     [DataContract]
     public class WritePartitionSnapshotGrpcModel
     {
-        [DataMember(Order = 1)] public List<string> Locations { get; set; }
+        [DataMember(Order = 1)] 
+        public List<string> Locations { get; set; }
 
-        [DataMember(Order = 2)] public string TableName { get; set; }
+        [DataMember(Order = 2)] 
+        public string TableName { get; set; }
 
-        [DataMember(Order = 3)] public string PartitionKey { get; set; }
+        [DataMember(Order = 3)] 
+        public string PartitionKey { get; set; }
 
-        [DataMember(Order = 4)] public bool PersistTable { get; set; }
+        [DataMember(Order = 4)] 
+        public bool PersistTable { get; set; }
 
-        [DataMember(Order = 5)] public byte[] Content { get; set; }
+        [DataMember(Order = 5)] 
+        public byte[] Content { get; set; }
 
-        [DataMember(Order = 6)] public SyncGrpcHeader[] Headers { get; set; }
+        [DataMember(Order = 6)] 
+        public SyncGrpcHeader[] Headers { get; set; }
 
+    }
+    
+    [DataContract]
+    public class WriteRowsUpdateGrpcModel
+    {
+        [DataMember(Order = 1)] 
+        public List<string> Locations { get; set; }
+
+        [DataMember(Order = 2)] 
+        public string TableName { get; set; }
+
+        [DataMember(Order = 3)] 
+        public byte[] RowsContent { get; set; }
+
+        [DataMember(Order = 4)] 
+        public SyncGrpcHeader[] Headers { get; set; }
     }
 
     [DataContract]
