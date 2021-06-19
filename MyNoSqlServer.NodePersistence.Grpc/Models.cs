13a619f2f7ec15f4b42860116e361309a51ec7b3
Andrey <amigin@gmail.com>
2021-06-19 12:46:29 +0300

    Modes Improvments

--- MyNoSqlServer.NodePersistence.Grpc/Models.cs
+++ MyNoSqlServer.NodePersistence.Grpc/Models.cs
@@ -9,16 +9,16 @@ namespace MyNoSqlServer.NodePersistence.Grpc
     public class WriteTableSnapshotGrpcModel
     {
         [DataMember(Order = 1)] 
-        public List<string> Locations { get; set; }
-
-        [DataMember(Order = 2)] 
         public string TableName { get; set; }
-
+        
+        [DataMember(Order = 2)] 
+        public List<string> Locations { get; set; }
+        
         [DataMember(Order = 3)] 
-        public bool PersistTable { get; set; }
+        public TableAttributesGrpcData Attributes { get; set; }
 
         [DataMember(Order = 4)] 
-        public byte[] Content { get; set; }
+        public byte[] TableSnapshot { get; set; }
 
         [DataMember(Order = 5)] 
         public SyncGrpcHeader[] Headers { get; set; }
@@ -28,22 +28,17 @@ namespace MyNoSqlServer.NodePersistence.Grpc
     public class WritePartitionSnapshotGrpcModel
     {
         [DataMember(Order = 1)] 
+        public string TableName { get; set; }
+        
+        [DataMember(Order = 2)] 
         public List<string> Locations { get; set; }
 
-        [DataMember(Order = 2)] 
-        public string TableName { get; set; }
-
         [DataMember(Order = 3)] 
-        public string PartitionKey { get; set; }
-
-        [DataMember(Order = 4)] 
-        public bool PersistTable { get; set; }
-
-        [DataMember(Order = 5)] 
-        public byte[] Content { get; set; }
-
-        [DataMember(Order = 6)] 
         public SyncGrpcHeader[] Headers { get; set; }
+        
+        [DataMember(Order = 4)] 
+        public byte[] PartitionsToBeInitialized { get; set; }
+
 
     }
     
@@ -83,10 +78,18 @@ namespace MyNoSqlServer.NodePersistence.Grpc
         public string TableName { get; set; }
 
         [DataMember(Order = 2)] 
+        public TableAttributesGrpcData Attributes { get; set; }
+
+    }
+    
+    [DataContract]
+    public class TableAttributesGrpcData
+    {
+        [DataMember(Order = 1)] 
         public bool Persist { get; set; }
 
-        [DataMember(Order = 3)] 
-        public int MaxPartitionsAmount { get; set; }
+        [DataMember(Order = 2)] 
+        public int MaxPartitionsAmount { get; set; }  
     }
 
 
