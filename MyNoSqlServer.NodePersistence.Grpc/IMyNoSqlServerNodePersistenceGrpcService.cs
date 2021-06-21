38913a7d049503e821706597f3dcea79ee1c362b
Andrey <amigin@gmail.com>
2021-06-21 09:52:57 +0300

    Optimizing Grpc Persistence Flow

--- MyNoSqlServer.NodePersistence.Grpc/IMyNoSqlServerNodePersistenceGrpcService.cs
+++ MyNoSqlServer.NodePersistence.Grpc/IMyNoSqlServerNodePersistenceGrpcService.cs
@@ -10,34 +10,11 @@ namespace MyNoSqlServer.NodePersistence.Grpc
         [OperationContract(Action = "Ping")]
         ValueTask<PingGrpcResponse> PingAsync(PingGrpcRequest request);
 
-        [OperationContract(Action = "SaveTableSnapshot")]
-        ValueTask SaveTableSnapshotAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
+        [OperationContract(Action = "SyncTransactions")]
+        ValueTask SyncTransactionsAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
         
-        [OperationContract(Action = "SaveTableSnapshotCompressed")]
-        ValueTask SaveTableSnapshotCompressedAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
-        
-        [OperationContract(Action = "SavePartitionSnapshot")]
-        ValueTask SavePartitionSnapshotAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
-        
-        [OperationContract(Action = "SavePartitionCompressedSnapshot")]
-        ValueTask SavePartitionCompressedSnapshotAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
-        
-        [OperationContract(Action = "SaveRowsSnapshot")]
-        ValueTask SaveRowsSnapshotAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
-        
-        [OperationContract(Action = "SaveRowsCompressedSnapshot")]
-        ValueTask SaveRowsCompressedSnapshotAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
-        
-        
-        [OperationContract(Action = "DeleteRows")]
-        ValueTask DeleteRowsAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
-
-        
-        [OperationContract(Action = "DeleteTablePartitions")]
-        ValueTask DeleteTablePartitionsAsync(DeleteTablePartitionGrpcRequest request);
-
-        [OperationContract(Action = "SetTableAttributes")]
-        ValueTask SetTableAttributesAsync(SetTableAttributesGrpcRequest request);
+        [OperationContract(Action = "SyncTransactions")]
+        ValueTask SyncTransactionsCompressedAsync(IAsyncEnumerable<PayloadWrapperGrpcModel> payloads);
 
         [OperationContract(Action = "GetTables")]
         IAsyncEnumerable<ReadTableAttributeGrpcModel> GetTablesAsync();
